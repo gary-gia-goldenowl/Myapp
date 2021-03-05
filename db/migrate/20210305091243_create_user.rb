a1869a51bf72855910ad69e7e4ddd1541f8c61db
gary <gary-gia-goldenowl@gmail.com>
2021-03-05 17:44:18 +0700

    add model user and post

--- db/migrate/20210305091243_create_user.rb
+++ db/migrate/20210305091243_create_user.rb
@@ -1,7 +1,15 @@
 class CreateUser < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-
+      t.string  :first_name
+      t.string  :last_name
+      t.string  :email
+      t.string  :title
+      t.string  :avatar
+      t.string  :gender
+      t.numeric  :age
+      t.string  :role
+      
       t.timestamps
     end
   end
