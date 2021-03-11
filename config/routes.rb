76000e1d002826491180d400733c63bc7b4eba55
gary <gary-gia-goldenowl@gmail.com>
2021-03-11 15:32:09 +0700

    Create controller view route for user , post , category

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/mypost", to: "posts#list"
+  get "/myuser", to: "users#list"
+  get "/mycategory", to: "categories#list"
 end
